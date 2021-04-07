06a3843bd6a69ae05b8b75b0dfffe5aa9a3fa639
Chase <56274352+PrismaticCode@users.noreply.github.com>
2021-04-06 21:56:00 -0500

    Update main.py

--- NaiveBayes_TextClassification/src/main.py
+++ NaiveBayes_TextClassification/src/main.py
@@ -9,6 +9,7 @@ arguments = list(sys.argv)
 
 data_set_name = str(arguments[1])
 
+
 def main():
     """
     This is the main function which is used to run all the algorithms
@@ -19,12 +20,6 @@ def main():
     print("The Precision is", evaluation_metrics[1])
     print("The Recall is", evaluation_metrics[2])
     print("The F1 Score is", evaluation_metrics[3])
-
-    try:
-        value = evaluation_metrics[4]
-        print("The selected value of lambda is", value)
-    except:
-        i = 0
 
 
 if __name__ == "__main__":
